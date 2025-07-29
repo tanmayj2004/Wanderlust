77d44347a319c695a4b60395caca03bd9926849f
Tanmay Jain <tanmayjain101011200@gmail.com>
2025-07-29 10:18:04 +0530

    update files

--- routes/listing.js
+++ routes/listing.js
@@ -8,7 +8,7 @@ const multer  = require('multer')
 const {storage}=require("../cloudConfig.js");
 const upload = multer({ storage })
 
-router.route("./listings/index.ejs")
+router.route("./")
 .get( wrapAsync(listingController.index))
 .post(  isLogedIN,upload.single("listing[image]"), wrapAsync(listingController.createListing));
 
